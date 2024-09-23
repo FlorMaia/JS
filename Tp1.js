c57e32447f11afaaf3390b8426099c4be7b47908
FlorMaia <166556922+FlorMaia@users.noreply.github.com>
2024-09-22 21:56:35 -0300

    Add files via upload

---
+++ Tp1.js
@@ -0,0 +1,10 @@
+
+
+let nombre = "Valen";
+let apellido = "Lopez";
+let edad = 19;
+let Cuidad_natal= "rio grande";
+console.log("Nombre:", nombre);
+console.log("Apellido:", apellido);
+console.log("Edad:", edad);
+console.log("Ciudad natal:", ciudad_natal);
